acfdc0ca53059e94beac7c5a0b14ab2e0623ced1
Tonylades <135992103+Tonylades@users.noreply.github.com>
2023-06-09 11:57:29 -0300

    Add files via upload

--- script/c999999992.lua
+++ script/c999999992.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,{id,1})
+	e1:SetCountLimit(id,1)
 	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
@@ -24,7 +24,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,{id,1})
+	e2:SetCountLimit(id,1)
 	e2:SetTarget(s.target)
 	e2:SetOperation(s.lvop)
 	c:RegisterEffect(e2)
@@ -48,8 +48,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+--Change Levels
 function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_TUNER)
+	return c:IsFaceup() and c:IsType(TYPE_TUNER) and c:GetLevel()~=2
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
@@ -57,14 +58,35 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,99,nil)
 end
-function s.lvop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+local g=Duel.GetTargetCards(e):Filter(Card.IsFaceup,nil)
+	for tc in aux.Next(g) do
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetCode(EFFECT_CHANGE_LEVEL)
+		e3:SetValue(2)
+		tc:RegisterEffect(e3)
+end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-	local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_CHANGE_LEVEL)
-	e3:SetValue(2)
-	tc:RegisterEffect(e3)
+	if tc:IsRelateToEffect(e) then
+		local p=tc:GetControler()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
+		--Clock Lizard check
+		aux.addTempLizardCheck(e:GetHandler(),tp,s.lizfilter)
+end
+function s.splimit(e,c)
+	return not (c:IsType(TYPE_SYNCHRO)) and c:IsLocation(LOCATION_EXTRA)
+end
+function s.lizfilter(e,c)
+	return not (c:IsOriginalType(TYPE_SYNCHRO))
 end
